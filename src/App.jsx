ba043e8a4c8307889d51b4581b7ff075424bd22e
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-01-30 15:15:53 +0700

    Feat: remove app.css react default style; add a lil bit styling to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,21 @@
 import { Link } from "react-router-dom"
 
 const Navbar = () => {
-    return (<nav>
-        <ul>
+    return (
+    <nav className="flex justify-center items-center gap-4 bg-black/40 text-white h-16">
+        <ul className="flex justify-between items-center gap-4">
             <li>
-                <Link to="/">Home</Link>
+                <Link to="/" className="text-white/40 hover:text-white">Home</Link>
             </li>
             <li>
-                <Link to="/about">About</Link>
+                <Link to="/about" className="text-white/40 hover:text-white">About</Link>
             </li>
             <li>
-                <Link to="/contact">Contact Us</Link>
+                <Link to="/contact" className="text-white/40 hover:text-white">Contact Us</Link>
             </li>
         </ul>
-    </nav>)
+    </nav>
+    )
 }
 
 export default Navbar
